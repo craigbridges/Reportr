d71cf3a1d1a75ec13bfa35b60c69ded132c989a7
Craig Bridges <craig@codechange.co.uk>
2018-06-19 22:18:15 +0100

    Assembly info

--- src/Reportr.Integrations.Autofac/Properties/AssemblyInfo.cs
+++ src/Reportr.Integrations.Autofac/Properties/AssemblyInfo.cs
@@ -5,10 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("Reportr Autofac Integration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Reportr.Integrations.Autofac")]
+[assembly: AssemblyCopyright("Copyright © Craig Bridges 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
