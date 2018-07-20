6f40f03284f63e4ed03d3c613431db9b550dfdca
Craig Bridges <craig.bridges@yahoo.co.uk>
2018-07-20 11:21:49 +0100

    Registered EfDataSource

---
+++ src/Reportr.Data.Entity/EntityTypeRegister.cs
@@ -0,0 +1,27 @@
+﻿namespace Reportr.Data.Entity
+{
+    using Reportr.IoC;
+    using System.Collections.Generic;
+    
+    /// <summary>
+    /// Represents the data entity type register implementation
+    /// </summary>
+    public sealed class EntityTypeRegister : ITypeRegister
+    {
+        /// <summary>
+        /// Gets a collection of all registered types
+        /// </summary>
+        /// <returns>A collection of registered types</returns>
+        public IEnumerable<RegisteredType> GetRegisteredTypes()
+        {
+            return new List<RegisteredType>()
+            {
+                new RegisteredType
+                (
+                    typeof(IDataSource),
+                    typeof(EfDataSource)
+                )
+            };
+        }
+    }
+}
