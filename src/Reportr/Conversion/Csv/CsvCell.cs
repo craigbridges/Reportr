827315650337d51f69bd74421ef27a28696c5a73
Craig Bridges <craig.bridges@yahoo.co.uk>
2018-08-28 17:24:15 +0100

    CSV export progress

---
+++ src/Reportr/Conversion/Csv/CsvCell.cs
@@ -0,0 +1,25 @@
+﻿namespace Reportr.Conversion.Csv
+{
+    /// <summary>
+    /// Represents a single Comma Separated (CSV) cell
+    /// </summary>
+    public class CsvCell
+    {
+        /// <summary>
+        /// Constructs the cell with a value
+        /// </summary>
+        /// <param name="value">The cell value</param>
+        public CsvCell
+            (
+                object value
+            )
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the CSV cell value
+        /// </summary>
+        public object Value { get; }
+    }
+}
